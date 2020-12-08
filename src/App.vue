715987f9069c990d91def147ea7fa0df5749d505
Shine-S <534661004@qq.com>
2020-12-08 16:48:14 +0800

    解决冲突

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,5 @@
 <template>
-<<<<<<< HEAD
   <router-view></router-view>
-=======
-<<<<<<< HEAD
-  <div>
-    <router-view></router-view>
-  </div>
-=======
-  <router-view></router-view>
->>>>>>> 3796b95e1ef4f9c0003b2a79d1c12ef211c50c7f
->>>>>>> 9a5ee7a42d9d0f826d20c8b83a2766b1afc173a9
 </template>
 
 <script>
@@ -18,8 +8,6 @@ export default {
 };
 </script>
 
-<<<<<<< HEAD
+
 <style lang="less" scoped></style>
-=======
-<style lang="less" scoped></style>
->>>>>>> 9a5ee7a42d9d0f826d20c8b83a2766b1afc173a9
+
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,36 +1,17 @@
-<<<<<<< HEAD
+import Order from "@/views/Order";
+import Search from "@/views/Search";
+import Detail from "@/views/Detail";
+const Product = () => import("@/views/Product");
 
-
-import Order from '@/views/Order'
-import Search from '@/views/Search'
-
-export default  [
- {
-    path:'/order',
-    component:Order,
-              
-  },
- {
-    path:'/search',
-    component:Search,
-              
-  },
-
- 
-]
-=======
-<<<<<<< HEAD
-import Detail from '@/views/Detail'
-
-export default[
-    {
-        path:'/detail',
-        component:Detail
-    }
-]
-=======
-const Product = () => import("@/view/product");
 export default [
+  {
+    path: "/order",
+    component: Order,
+  },
+  {
+    path: "/search",
+    component: Search,
+  },
   {
     path: "/product",
     component: Product,
@@ -39,6 +20,8 @@ export default [
     path: "/",
     redirect: "/product",
   },
+  {
+    path: "/detail",
+    component: Detail,
+  },
 ];
->>>>>>> 3796b95e1ef4f9c0003b2a79d1c12ef211c50c7f
->>>>>>> 9a5ee7a42d9d0f826d20c8b83a2766b1afc173a9
