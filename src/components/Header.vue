5c17d7875e8b8848b0ebe2bdba73c84dc84426b4
Shine-S <534661004@qq.com>
2020-12-08 09:36:25 +0800

    添加reses.css文件 header组件的编写

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,8 @@
 <template>
-  <div>
-    头部组件
+  <div class="top">
+    <div class="headerContainer clearfix">
+      <div class="rightNav"></div>
+    </div>
   </div>
 </template>
 
@@ -10,4 +12,16 @@ export default {
 };
 </script>
 
-<style lang="less" scoped></style>
+<style lang="less" scoped>
+.top {
+  height: 48px;
+  background-color: #333;
+  font-size: 14px;
+  position: relative;
+  z-index: 105;
+  .headerContainer {
+    width: 1080px;
+    margin: 0 auto;
+  }
+}
+</style>
