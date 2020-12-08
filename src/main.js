ee7deb7f4ceab4aa1f8745c03b76ac138f3c8def
Shine-S <534661004@qq.com>
2020-12-08 16:59:25 +0800

    解决冲突

--- src/main.js
+++ src/main.js
@@ -1,14 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-import FixedBar from '@/components/FixedBar'
-Vue.config.productionTip = false
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import FixedBar from "@/components/FixedBar";
+Vue.config.productionTip = false;
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 
 import SliderLoop from "@/components/SliderLoop";
+import Header from "@/components/Header";
+import Top from "@/components/Top";
+import Footer from "@/components/Footer";
 
 //注册全局组件
 //低部组件
@@ -19,6 +22,7 @@ Vue.component("Header", Header);
 Vue.component("Top", Top);
 //轮播图组件
 Vue.component("SliderLoop", SliderLoop);
+Vue.component("FixedBar", FixedBar);
 
 Vue.use(ElementUI);
 new Vue({
