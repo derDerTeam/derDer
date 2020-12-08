019eead99cb81cdaa801f325df9913b35c2a4f01
Shine-S <534661004@qq.com>
2020-12-08 08:47:35 +0800

    封装axios mockjs安装 store模块化

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
+  beforeCreate() {
+    //全局事件总线
+    Vue.prototype.$bus = this;
+  },
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
