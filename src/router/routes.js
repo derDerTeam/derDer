127896f69db3909bf8ac165f4bc5e6a400852045
zhutianzun <zhutianzun@ysstech.com>
2020-12-08 17:04:42 +0800

    'test'

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 
 import Order from '@/views/Order'
@@ -18,8 +18,7 @@ export default  [
 
  
 ]
-=======
-<<<<<<< HEAD
+
 import Detail from '@/views/Detail'
 
 export default[
@@ -28,7 +27,6 @@ export default[
         component:Detail
     }
 ]
-=======
 const Product = () => import("@/view/product");
 export default [
   {
@@ -40,5 +38,4 @@ export default [
     redirect: "/product",
   },
 ];
->>>>>>> 3796b95e1ef4f9c0003b2a79d1c12ef211c50c7f
->>>>>>> 9a5ee7a42d9d0f826d20c8b83a2766b1afc173a9
+
