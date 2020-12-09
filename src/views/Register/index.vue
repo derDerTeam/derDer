e5f3bf0cf1fceda0cc8eff81b5a75f9b4229a2b2
Shine-S <534661004@qq.com>
2020-12-09 23:56:49 +0800

    shine over

--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -1,42 +1,38 @@
 <template>
-	<div id="wrapper">
-		<!-- 主体 -->
-		<div class="main-content">
-
-		</div>
-		<Lfooter></Lfooter>
-	</div>
+  <div id="wrapper">
+    <!-- 主体 -->
+    <div class="main-content"></div>
+    <Lfooter></Lfooter>
+  </div>
 </template>
 
 <script>
-	import Lfooter from "./components/Lfooter";
-	export default {
-		name: "Login",
-		data() {
-			return {
-				qrCode: true,
-			}
-		},
-		methods: {
-			
-		},
-		components: {
-			userLogin,
-			Lfooter,
-			qrCode
-		}
-	}
+import Lfooter from "./components/Lfooter";
+export default {
+  name: "Login",
+  data() {
+    return {
+      qrCode: true,
+    };
+  },
+  methods: {},
+  components: {
+    userLogin,
+    Lfooter,
+    qrCode,
+  },
+};
 </script>
 
 <style lang="less" scoped>
-	#wrapper {
-		background-color: #F5F5F5;
+#wrapper {
+  background-color: #f5f5f5;
 
-		.main-content {
-			background-color: #fff;
-			max-width: 854px;
-			height: 100%;
-			margin: 0 auto 20px;
-		}
-	}
+  .main-content {
+    background-color: #fff;
+    max-width: 854px;
+    height: 100%;
+    margin: 0 auto 20px;
+  }
+}
 </style>
