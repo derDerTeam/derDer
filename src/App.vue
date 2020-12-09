390fb02901363c0db4f0eb99ad8bfcce98889ead
unknown <xinyan@qq.com>
2020-12-09 14:29:27 +0800

    iop

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,20 @@
 <template>
-  <!-- <router-view></router-view> -->
-   <!--使用组件-->
-    <Pay />
- 
+  <div>
+    <router-view></router-view>
+    <!--使用组件-->
+    <Pay />
+  </div>
 </template>
-
 
 <script>
 // 引入组件
-import Pay from '@/views/Pay/index'
-export default {
-  name: "App",
-  // 注册组件
-  components:{
-    Pay
- 
-  }
+import Pay from "@/views/Pay/index";
+export default {
+  name: "App", // 注册组件
+  components: {
+    Pay,
+  },
 };
 </script>
 
-<style lang="less" scoped>
-
-</style>
-
+<style lang="less" scoped></style>
