2a60e720131f65cddaf554c5d2378306ace0c27f
clatter65 <clatter66@sina.com>
2020-12-09 16:54:35 +0800

    asd

--- src/views/Register/index.vue
+++ src/views/Register/index.vue
@@ -1,15 +1,42 @@
 <template>
-	<div>
-		注册
+	<div id="wrapper">
+		<!-- 主体 -->
+		<div class="main-content">
+
+		</div>
+		<Lfooter></Lfooter>
 	</div>
 </template>
 
 <script>
+	import Lfooter from "./components/Lfooter";
 	export default {
-		
+		name: "Login",
+		data() {
+			return {
+				qrCode: true,
+			}
+		},
+		methods: {
+			
+		},
+		components: {
+			userLogin,
+			Lfooter,
+			qrCode
+		}
 	}
 </script>
 
 <style lang="less" scoped>
+	#wrapper {
+		background-color: #F5F5F5;
 
+		.main-content {
+			background-color: #fff;
+			max-width: 854px;
+			height: 100%;
+			margin: 0 auto 20px;
+		}
+	}
 </style>
